cca2c688efeecbe1642f8c80554e011cc21afa74
Pierre Schweitzer <pierre@reactos.org>
2013-12-22 15:38:43 +0000

    [SYSREG2] Update header
    svn path=/trunk/sysreg2/; revision=2075

--- console.c
+++ console.c
@@ -4,7 +4,7 @@
  * PURPOSE:     Processing the incoming debugging data
  * COPYRIGHT:   Copyright 2008-2009 Christoph von Wittich <christoph_vw@reactos.org>
  *              Copyright 2009 Colin Finck <colin@reactos.org>
- *              Copyright 2012 Pierre Schweitzer <pierre@reactos.org>
+ *              Copyright 2012-2013 Pierre Schweitzer <pierre@reactos.org>
  */
 
 #include "sysreg.h"
